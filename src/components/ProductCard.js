575872b857814c6da70bc46c552e81a8209c6b76
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-02-05 23:47:49 +0600

    Add To Cart Feature Added

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+
+  const dispatch = useDispatch()
 
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
-      key="{product._id}"
-    >
+      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'>
       <div className='h-52 w-52 mx-auto'>
-        <img src="{product.image}" alt="{product.model}" />
+        <img src={product.image} alt={product.model} />
       </div>
-      <h1 className='font-bold text-center'>Model Product</h1>
-      <p className='text-center font-semibold mb-3'>Rating: Product Rating</p>
+      <h1 className='font-bold text-center'>{product.model}</h1>
+      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
       <div className=' flex-1'>
         <ul className='space-y-2'>
-          <li className='text-sm '>vbcb</li>
+          <li className='text-sm '><b>Price: </b>{product.price}</li>
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
         <button
-          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+          onClick={() => dispatch({ type: ADD_TO_CART, payload: product })}
+        >
           Add to cart
         </button>
         <button
